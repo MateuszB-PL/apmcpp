7ad2e3fada6199852ab17882f9693d41e29ec26e
Mateusz Burcon <mateusz0burcon@gmail.com>
2024-05-26 20:34:48 +0200

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,28 @@
 #include "mkfile.cpp"
 //main
 
+class programInfo {
+    public:
+        std::string version = "1.0.0";
+        std::string cppver;
+        std::string chkCppVer(){
+            if (__cplusplus == 202101L) cppver = "C++23";
+            else if (__cplusplus == 202002L) cppver = "C++20";
+            else if (__cplusplus == 201703L) cppver = "C++17";
+            else if (__cplusplus == 201402L) cppver = "C++14";
+            else if (__cplusplus == 201103L) cppver = "C++11";
+            else if (__cplusplus == 199711L) cppver = "C++98";
+            else cppver = "ERROR: version not in db";
+            return cppver;
+        }
+};
+
 int main(int argc, char *argv[]) {
+    programInfo programInfo;
     if (getuid() != 0){ std::cout<< vars.prefix << "Please run as root"; exit(1);}
     for(;;)
     {
-        switch(getopt(argc, argv, "iu:s:he")) // note the colon (:) to indicate that 'n' has a parameter and is not a switch
+        switch(getopt(argc, argv, "iu:s:hev")) // note the colon (:) to indicate that 'n' has a parameter and is not a switch
         {
         case 'i':
             std::cout << vars.prefix <<"Installing from APPCONF"<<std::endl;
@@ -24,6 +41,9 @@ int main(int argc, char *argv[]) {
             continue;
         case 'e':
             gen_example_appconf();
+            continue;
+        case 'v':
+            std::cout<< "C++ build version: "<< __cplusplus << " or "<< programInfo.chkCppVer() <<" | APM - App Package Manager version: "<< programInfo.version <<std::endl;
             continue;
         case '?':
         case 'h':
